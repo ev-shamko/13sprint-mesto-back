ba51fbfefd5ae0b88fef7deb6652d51a1f5293d8
ev-shamko <ev-shamko@yandex.ru>
2020-07-30 16:20:37 +0300

    удалён фронтенд, удалён лишний код из app.js

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 const express = require('express');
-// const path = require('path'); // динамически собирает путь до файлов
 const mongoose = require('mongoose'); // ODM пакет для взаимодействия с mongoDB
 const bodyParser = require('body-parser'); // внимание! обязателен! И ниже его app.use -аем дважды
 
-// импорт роутов для карточек и базы юзеров
-const usersRouter = require('./routes/users');
-const cardsRouter = require('./routes/cards');
+/* ***************************** */
 
 const { PORT = 3000 } = process.env;
-
 const app = express();
+
+/* ***************************** */
 
 // эти две строчки обязательные. Они собираюют из пакетов объект req.body
 // без них req.body = undefined
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+/* ***************************** */
+/* **************** Соединение с локальной БД ********************** */
+
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
+
+/* ***************************** */
+/* **************** Автозалогинивание ********************** */
 
 // это временное решение: мы захардкодили идентификатор пользователя
 // оно нужно затем, чтобы у карточек в Mesto был автор
@@ -33,10 +37,12 @@ app.use((req, res, next) => {
   next();
 });
 
-// УДАЛЕНО
-// делаем папку public публичной и корнем проекта,
-// поэтому ссылки дальше пишем относительно папки public
-// app.use(express.static(path.join(__dirname, 'public')));
+/* ***************************** */
+/* **************** РОУТЫ ********************** */
+
+// импорт роутов для карточек и базы юзеров
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
 
 // задействуем роуты для юзеров и карточек
 app.use('/users', usersRouter);
@@ -47,6 +53,8 @@ app.use('/cards', cardsRouter);
 app.use('/', (req, res) => {
   res.status(404).json({ message: 'Запрашиваемый ресурс не найден. Возможно, вы обращаетесь к фронтенду, который в данной итерации отключён' });
 });
+
+/* ********************************************* */
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
